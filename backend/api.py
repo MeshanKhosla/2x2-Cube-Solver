3693aa6b2620bde709cddec3a2976cf2b1bd55c4
meshankhosla <meshankhosla@gmail.com>
2022-01-16 17:14:29 -0800

    Add cors to flask app

--- backend/api.py
+++ backend/api.py
@@ -1,19 +1,29 @@
 from flask import Flask, request
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 from scripts.main import generate_solution, load_graph 
 
-app = Flask('__name__')
+app = Flask(__name__,static_folder='frontend/build',static_url_path='')
 
 graph = None
+CORS(app)
+
 @app.route("/setup-graph")
+@cross_origin()
 def setup_graph():
 	global graph
 	graph = load_graph()
 	return { 'status' : 200 }
 
 @app.route("/get-solution")
+@cross_origin()
 def get_solution():
 	initial_state = request.headers['initial_state']
 	return generate_solution(graph, initial_state)
 
+@app.route('/')
+def serve():
+	return send_from_directory(app.static_folder, 'index.html')
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
